71ec01fbac1ca78c806455b99ce7a25db5bbaa70
Bruno Pastre <pastre68@gmail.com>
2020-09-02 02:07:14 -0300

    Locks portrait mode

--- JamesEvaluationUITests/JamesEvaluationUITests.swift
+++ JamesEvaluationUITests/JamesEvaluationUITests.swift
@@ -17,12 +17,6 @@ class JamesEvaluationUITests: XCTestCase {
         app.launch()
         continueAfterFailure = false
     }
-
-    override func tearDownWithError() throws {
-        
-        
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
     
     func testHomeScreen() {
         
